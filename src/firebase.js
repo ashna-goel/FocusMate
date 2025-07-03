56af052d5ca2a74507292cd340ced4bc4bdbd54b
Ashna Goel <116701993+ashna-goel@users.noreply.github.com>
2025-07-02 19:27:23 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAIB4ZMfUkZm0_RYV8Nqkvj_36L1LM1rE8",
   authDomain: "focusmate-da088.firebaseapp.com",
@@ -11,5 +10,4 @@ const firebaseConfig = {
   measurementId: "G-4F42EVSN9Q"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
